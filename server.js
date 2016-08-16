bcefef9fc4484ce7f85a57a90875125202e249a7
Ryan Mattson <ryansmattson@gmail.com>
2016-08-16 15:46:53 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+
+
+
+
+
+var server = app.listen(3000, function(){
+  var port = server.address().port;
+  console.log("Listening on port", port);
+})
